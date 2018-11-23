3b76ffc65d868ef7e63709ee6875515f0e5490b8
R4my <44817847+R4my@users.noreply.github.com>
2018-11-23 20:00:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -436,8 +436,6 @@ client.on('message', message => {
   **__$cl__** تـقـفـيل الـشـات
   -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
   **__$op__** فـتـح الـشـات
-  -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-  **__$cc__** يسويلك الوان بالعدد الي تبيه
   -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-`)
      message.author.sendEmbed(embed)
      }
@@ -1279,22 +1277,7 @@ console.log('[38ab] Send By: ' + message.author.username)
   }
 
 
- client.on('message', ra3d => {
-var prefix = "$";
-                        let args = ra3d.content.split(" ").slice(1).join(" ")
-if(ra3d.content.startsWith(prefix + 'cc')) {
-    if(!args) return ra3d.channel.send('`يرجي اختيار كم لون `');
-             if (!ra3d.member.hasPermission('MANAGE_ROLES')) return ra3d.channel.sendMessage('`**⚠ | `[MANAGE_ROLES]` لا يوجد لديك صلاحية**'); 
-              ra3d.channel.send(`**✅ |تم انشاء __${args}__ لون**`);
-                  setInterval(function(){})
-                    let count = 0;
-                    let ecount = 0;
-          for(let x = 1; x < `${parseInt(args)+1}`; x++){
-            ra3d.guild.createRole({name:x,
-              color: 'RANDOM'})
-              }
-            }
-       });
+
 
 });
 client.login(process.env.BOT_TOKEN);
